272f7ff8b9b9d73cf4b4794ae2deb470247a5df3
suhaimiannuar <suhaimi150@gmail.com>
2017-12-20 12:24:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/suhaimi/postjson/Person.java
@@ -0,0 +1,38 @@
+package com.example.suhaimi.postjson;
+
+/**
+ * Created by suhaimi on 12/20/17.
+ *
+ */
+
+public class Person {
+
+    private String name;
+    private String country;
+    private String twitter;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+}
